ca429dc6ff5d9827333d7f7d2fcebe0cb1c9eb02
r4g <philip.tresp@googlemail.com>
2022-09-26 09:53:57 +0200

    solved 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let fahrenheit = celsius * 1.8 + 32;
+  return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
