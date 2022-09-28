be9cec57edde72ea1b0a97fc496f84c6838a6c8a
r4g <philip.tresp@googlemail.com>
2022-09-28 12:59:55 +0200

    changes

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,11 +2,7 @@ const leapYears = function (year) {
   if (year % 400 === 0) {
     return true;
   }
-  if (year % 4 === 0 && year % 100 != 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return year % 4 === 0 && year % 100 !== 0;
 };
 
 // Do not edit below this line
